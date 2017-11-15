cd223fdd189b2af2bfaccedd9bad8f81dcbd0d9a
4010c5 <4010c5@coco.ece.uprm.edu>
2017-11-15 16:55:51 -0400

    Commented all of the draw methods and added a new one that draws a circle that always stays in the middle off the frame with a size of (55, 55). Also created new variables to represent the middle of the frame.

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -20,17 +20,33 @@ public class MyPanelClass extends JPanel {
                         int y2 = getHeight() - myInsets.bottom - 1;
                         int width = x2 - x1;
                         int height = y2 - y1;
+                        int centerWidth = (getWidth() / 2) - 27;
+                        int centerHeight = (getHeight() / 2) - 27;
  
                         //Paint the background
                         g.setColor(Color.BLUE);
                         g.fillRect(x1, y1, width + 1, height + 1);
                         
-                        //Draw a border
-                        g.setColor(Color.RED);
-                        g.drawRect(x1, y1, width, height);
+//                        //Draw a border
+//                        g.setColor(Color.RED);
+//                        g.drawRect(x1, y1, width, height);
+//                        
+//                        //Draw another border
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1 + 20, y1 + 20, width - 40, height - 40);
                         
-                        //Draw another border
-                        g.setColor(Color.YELLOW);
-                        g.drawRect(x1 + 20, y1 + 20, width - 40, height - 40);
+//                        //Draw diagonal lines
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        //Draw diagonal lines
+//                        g.setColor(Color.GRAY);
+//                        g.drawLine(x2, y1, x1, y2);
+//                        
+                        //Draw an oval
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.drawOval(centerWidth, centerHeight, 55, 55);
+                        
+                        
             }
 }
